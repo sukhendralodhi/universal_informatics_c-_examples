4015ca21dd154012f24a34235eedfd33c9ce6f13
sukhendra <mpct5jan1998@gmail.com>
2023-02-28 22:47:13 +0530

    add one more file prime number

---
+++ prime_number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int checkPrime(int num) {
+  // 0, 1 and negative numbers are not prime
+  if(num < 2) {
+    return 0;
+  } else {
+    int x = num /2;
+    for(int i = 2; i<=x; i++) {
+      if(num % i == 0) {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
+int main() {
+  int a = 1, b = 1000;
+  for(int i = a; i <= b; i++) {
+    if(checkPrime(i)) {
+      
+    cout << i << endl;
+    }
+  }
+}
